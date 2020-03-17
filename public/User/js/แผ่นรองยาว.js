cc07b4dca5c7b227ff811b047a29fdc975775441
Teerayut36 <t.yis0204@gmail.com>
2020-03-17 21:55:54 +0700

    cart

--- public/User/js/แผ่นรองยาว.js
+++ public/User/js/แผ่นรองยาว.js
@@ -1,4 +1,3 @@
-
 var myCanvasUtil;
 var img = qs("#inpt-img");
 var can = qs("#out-can");
@@ -31,4 +30,13 @@ function handleFiles(files) {
         img.src = reader.result;
     }
 }
-
+function resize(which, max) {
+    var elem = document.getElementById(which);
+    if (elem == undefined || elem == null) return false;
+    if (max == undefined) max = 100;
+    if (elem.width > elem.height) {
+      if (elem.width > max) elem.width = max;
+    } else {
+      if (elem.height > max) elem.height = max;
+    }
+  }
